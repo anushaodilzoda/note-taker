9632150f2a9d17e188a66dcfeafa910ffe367629
anushaodilzoda <51028662+anushaodilzoda@users.noreply.github.com>
2020-02-05 23:27:39 -0500

    fixed a bug

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static(__dirname + "/public"));
 // routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
